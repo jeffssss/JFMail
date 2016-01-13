f7a714c4185eb055cb9c5988776fe6c4ee9205f6
jeffssss <467318976@qq.com>
2016-01-13 22:05:49 +0800

    init

---
+++ JFMail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFMail
+//
+//  Created by JiFeng on 16/1/13.
+//  Copyright © 2016年 jeffsss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JFMail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFMail
+//
+//  Created by JiFeng on 16/1/13.
+//  Copyright © 2016年 jeffsss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
